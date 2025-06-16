01027333c9dde5a24bd09c320d6d4464c8d1b49e
Sandrine BRIVAL <sb@MacBook-Air-de-Sandrine.local>
2025-06-16 10:51:28 +0200

    feat: add bs4 package to get the html product elements

--- books_shops_scraping.py
+++ books_shops_scraping.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 scrap_url = 'https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html'
 
@@ -8,4 +9,6 @@ status_code = r.status_code
 
 if status_code == 200:
     print('Successfully scraped')
-
+    soup = BeautifulSoup(r.content, 'html.parser')
+    soup.prettify()
+    print(soup.h1.text)
