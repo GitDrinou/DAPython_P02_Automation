9f9b0711ab72757507789c6ed8a59884c47e897f
Sandrine BRIVAL <brival.sandrine@gmail.com>
2025-06-16 15:46:44 +0200

    feat: create script for extract information for one product to CSV

--- extract_csv.py
+++ extract_csv.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from scraping import get_product_information
 
@@ -6,10 +7,16 @@ scrap_url = 'https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-hum
 r = requests.get(scrap_url)
 status_code = r.status_code
 
-
 if status_code == 200:
     print('Successfully scraped')
-    print(get_product_information(r.content, scrap_url))
+    headers = ['product_page_url','universal_product_code','title','price_including_tax','price_excluding_tax',
+               'number_available','product_description','category','review_rating','image_url']
+    data = [get_product_information(r.content, scrap_url)]
 
+    # extract to csv
+    with open('extract/one_product.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, delimiter=',',fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(data)
 else:
     print('Failed to scrape, the product at: ' + str(scrap_url))
