8541626cbd48c108080247af4cc5dc4816059285
Sandrine BRIVAL <brival.sandrine@gmail.com>
2025-06-17 09:30:01 +0200

    feat: add datetime to filename expprt

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import csv
 import requests
 from scraping import get_product_information
+from datetime import datetime
 
 scrap_url = 'https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html'
 
 r = requests.get(scrap_url)
 status_code = r.status_code
+
+current_date = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 if status_code == 200:
     print('Successfully scraped')
@@ -14,7 +17,7 @@ if status_code == 200:
     data = [get_product_information(r.content, scrap_url)]
 
     # extract to csv
-    with open('extract/one_product.csv', 'w', newline='', encoding='utf-8') as file:
+    with open('extract/extract_one_product_'+ current_date +'.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.DictWriter(file, delimiter=',',fieldnames=headers)
         writer.writeheader()
         writer.writerows(data)
