f870e216740e67d79c5e1ec929c96d08716cdd24
Sandrine BRIVAL <brival.sandrine@gmail.com>
2025-07-04 09:40:47 +0200

    refactor: change the process of error management

--- prompt.py
+++ prompt.py
@@ -1,12 +1,8 @@
 def ask_url():
-    while True:
-        url_to_scrape = input("Veuillez entrer l'URL de la page à scraper: ").strip()
-        if url_to_scrape.endswith(".html"):
-            print("ERREUR: l'URL ne doit pas se terminer avec la page html. Veuillez réessayer.")
-            continue
-
-        if not url_to_scrape.endswith("/"):
-            print("ERREUR: l'URL doit se terminer par un '/'. Veuillez réessayer.")
-            continue
-
+    url_to_scrape = input("Veuillez entrer l'URL de la page à scraper: ").strip()
+    if url_to_scrape.endswith(".html"):
+        return url_to_scrape.rsplit("/", 1)[0] + "/"
+    elif not url_to_scrape.endswith("/"):
+        return url_to_scrape + "/"
+    else:
         return url_to_scrape
