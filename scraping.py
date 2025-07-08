26c5e42315391aaa512685426e17f785b810bb82
Sandrine BRIVAL <brival.sandrine@gmail.com>
2025-07-08 11:04:37 +0200

    refactor: delete function in function

--- scraping.py
+++ scraping.py
@@ -11,6 +11,7 @@ class Rating(Enum):
     FOUR = 4
     FIVE = 5
 
+
 def get_content(page_url):
     """Get html from url"""
     r = requests.get(page_url)
@@ -19,38 +20,33 @@ def get_content(page_url):
     else:
         return print('Error: Unable to process the page.')
 
+def get_rating(rating):
+    """Get the rating value from a class name"""
+    value_rating = 0
+    if len(rating) > 1:
+        for enum in Rating:
+            if enum.name == rating[1].upper():
+                value_rating = enum.value
+    return value_rating
+
 def get_product_information(html, url, base_url):
     """Generate a dictionary of product information."""
     soup = BeautifulSoup(html, 'html.parser')
     soup.prettify()
 
-    def get_fields(name, tag = '', is_id = False, is_rating = False):
-        """Get the text for next sibling HTML element of selected tag or id"""
-        if is_id:
-            if soup.find(id=name) is not None:
-                return soup.find(id=name).find_next_sibling().string
-            return ''
-        elif is_rating:
-            rating = soup.find(tag, class_=name).get('class')
-            value_rating = 0
-            if len(rating) > 1:
-                for enum in Rating:
-                    if enum.name == rating[1].upper():
-                        value_rating = enum.value
-            return value_rating
-        else:
-            return soup.find(tag, string=name).find_next_sibling().string
-
-    upc = get_fields('UPC', 'th')
+    upc = soup.find('th', string='UPC').find_next_sibling().string
     title = soup.h1.string
-    price_with_tax = get_fields('Price (incl. tax)', 'th')
-    price_without_tax = get_fields('Price (excl. tax)', 'th')
-    availability = get_fields('Availability', 'th')
+    price_with_tax = soup.find('th', string='Price (incl. tax)').find_next_sibling().string
+    price_without_tax = soup.find('th', string='Price (excl. tax)').find_next_sibling().string
+    availability = soup.find('th', string='Availability').find_next_sibling().string
     price_amount_with_tax = f"£{float(price_with_tax[1:]):,.2f}"
     price_amount_without_tax = f"£{float(price_without_tax[1:]):,.2f}"
     number_available = re.findall(r'\d+', availability)[0]
     links = soup.find('ul', class_='breadcrumb').find_all('li')
     category = links[-2].find('a').string
+
+    rating = soup.find('p',class_='star-rating').get('class')
+
 
     images = soup.find_all('img')
     image_url = ''
@@ -66,9 +62,9 @@ def get_product_information(html, url, base_url):
         'price_including_tax': price_amount_with_tax,
         'price_excluding_tax': price_amount_without_tax,
         'number_available': number_available,
-        'product_description': get_fields('product_description', '', True),
+        'product_description': soup.find(id='product_description').find_next_sibling().string,
         'category': category,
-        'review_rating': get_fields('star-rating', 'p', False,True),
+        'review_rating': get_rating(rating),
         'image_url': base_url + image_url,
     }
 
